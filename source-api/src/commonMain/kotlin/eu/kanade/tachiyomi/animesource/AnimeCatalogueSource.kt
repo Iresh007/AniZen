4feef6e30389b0a3bc509717fc7f956f0066119f
salmanbappi <salmanbappi@users.noreply.github.com>
2026-09-15 22:51:11 +0600

    fix(source-api): inherit AnimeSource update implementations without deprecated async

--- source-api/src/commonMain/kotlin/eu/kanade/tachiyomi/animesource/AnimeCatalogueSource.kt
+++ source-api/src/commonMain/kotlin/eu/kanade/tachiyomi/animesource/AnimeCatalogueSource.kt
@@ -57,30 +57,6 @@ interface AnimeCatalogueSource : AnimeSource {
      * Returns the list of filters for the source.
      */
     override fun getFilterList(): AnimeFilterList
-
-    @Suppress("DEPRECATION")
-    override suspend fun getAnimeEpisodeUpdate(
-        anime: eu.kanade.tachiyomi.animesource.model.SAnime,
-        episodes: List<eu.kanade.tachiyomi.animesource.model.SEpisode>,
-        fetchDetails: Boolean,
-        fetchEpisodes: Boolean,
-    ): eu.kanade.tachiyomi.animesource.model.SAnimeEpisodeUpdate = kotlinx.coroutines.supervisorScope {
-        val asyncAnime = if (fetchDetails) kotlinx.coroutines.async { getAnimeDetails(anime) } else null
-        val asyncEpisodes = if (fetchEpisodes) kotlinx.coroutines.async { getEpisodeList(anime) } else null
-        eu.kanade.tachiyomi.animesource.model.SAnimeEpisodeUpdate(asyncAnime?.await() ?: anime, asyncEpisodes?.await() ?: episodes)
-    }
-
-    @Suppress("DEPRECATION")
-    override suspend fun getAnimeSeasonUpdate(
-        anime: eu.kanade.tachiyomi.animesource.model.SAnime,
-        seasons: List<eu.kanade.tachiyomi.animesource.model.SAnime>,
-        fetchDetails: Boolean,
-        fetchSeasons: Boolean,
-    ): eu.kanade.tachiyomi.animesource.model.SAnimeSeasonUpdate = kotlinx.coroutines.supervisorScope {
-        val asyncAnime = if (fetchDetails) kotlinx.coroutines.async { getAnimeDetails(anime) } else null
-        val asyncSeasons = if (fetchSeasons) kotlinx.coroutines.async { getSeasonList(anime) } else null
-        eu.kanade.tachiyomi.animesource.model.SAnimeSeasonUpdate(asyncAnime?.await() ?: anime, asyncSeasons?.await() ?: seasons)
-    }
     // ANZ <--
 
     // Should be replaced as soon as Anime Extension reach 1.5
